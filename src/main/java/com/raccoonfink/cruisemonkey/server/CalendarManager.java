7c569924a91f12041e1bcd8a379df57ce33538cf
Benjamin Reed <rangerrick@befunk.com>
2012-09-11 23:26:17 -0400

    create a calendar manager

---
+++ src/main/java/com/raccoonfink/cruisemonkey/server/CalendarManager.java
@@ -0,0 +1,105 @@
+package com.raccoonfink.cruisemonkey.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import com.raccoonfink.cruisemonkey.dao.CalendarVisitor;
+
+public class CalendarManager implements InitializingBean {
+	@Autowired
+	private URL m_url;
+
+	private CalendarVisitor m_visitor;
+	private ScheduledExecutorService m_executor = Executors.newSingleThreadScheduledExecutor();
+
+	public CalendarManager() {
+	}
+
+	public synchronized URL getUrl() {
+		return m_url;
+	}
+
+	public synchronized void setUrl(final URL url) {
+		m_url = url;
+	}
+	
+	public synchronized CalendarVisitor getVisitor() {
+		return m_visitor;
+	}
+	
+	public synchronized void setVisitor(final CalendarVisitor visitor) {
+		m_visitor = visitor;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(m_url);
+		Assert.notNull(m_visitor);
+
+		m_executor.schedule(getRunnable(), 5, TimeUnit.MINUTES);
+	}
+
+	private Runnable getRunnable() {
+		return new Runnable() {
+			@Override
+			public void run() {
+		        final URL url = getUrl();
+				try {
+					final Calendar calendar = getCalendar(url);
+					CalendarManager.visitCalendar(calendar, getVisitor());
+				} catch (final IOException e) {
+					e.printStackTrace();
+				} catch (final ParserException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void visitCalendar(final Calendar calendar, final CalendarVisitor visitor) {
+		visitor.begin();
+	
+	    for (final Component component : (List<Component>)calendar.getComponents()) {
+	        String name = component.getName();
+	
+	        if ("VEVENT".equals(name)) {
+	        	visitor.visitEvent(component);
+	        } else if ("VTIMEZONE".equals(name)) {
+	        	visitor.visitTimezone(component);
+	        } else {
+	            System.err.println("Warning: not sure how to handle " + name);
+	        }
+	    }
+	
+	    visitor.end();
+	}
+
+	private Calendar getCalendar(final URL url) throws IOException, ParserException {
+		final InputStream is = url.openStream();
+	    final CalendarBuilder builder = new CalendarBuilder();
+	    final Calendar calendar = builder.build(is);
+	    is.close();
+		return calendar;
+	}
+
+	public void updateNow() {
+		getRunnable().run();
+	}
+
+	
+}
--- src/test/java/com/raccoonfink/cruisemonkey/server/CalendarTest.java
+++ src/test/java/com/raccoonfink/cruisemonkey/server/CalendarTest.java
@@ -4,20 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
-
-import net.fortuna.ical4j.data.CalendarBuilder;
-import net.fortuna.ical4j.data.ParserException;
-import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Component;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.raccoonfink.cruisemonkey.dao.CalendarVisitor;
 import com.raccoonfink.cruisemonkey.dao.EventDao;
 import com.raccoonfink.cruisemonkey.dao.hibernate.HibernateEventDao;
 import com.raccoonfink.cruisemonkey.dao.hibernate.OfficialCalendarVisitor;
@@ -34,11 +25,10 @@ public class CalendarTest {
 	@Test
     public void testOfficialCalendarVisitor() throws Exception {
         // final URL url = new URL("https://www.google.com/calendar/ical/nh76o8dgn9d86b7n3p3uofg1q0%40group.calendar.google.com/public/basic.ics");
-        final URL url = new File("src/test/resources/before.ics").toURI().toURL();
-
-        final Calendar calendar = getCalendar(url);
-        final CalendarVisitor visitor = new OfficialCalendarVisitor();
-        visitCalendar(calendar, visitor);
+        final CalendarManager manager = new CalendarManager();
+        manager.setUrl(new File("src/test/resources/before.ics").toURI().toURL());
+        manager.setVisitor(new OfficialCalendarVisitor());
+        manager.updateNow();
         
         final List<Event> events = m_eventDao.findAll();
         assertEquals(2, events.size());
@@ -48,10 +38,10 @@ public class CalendarTest {
 	@Test
     public void testOfficialCalendarVisitorUpdate() throws Exception {
         // final URL url = new URL("https://www.google.com/calendar/ical/nh76o8dgn9d86b7n3p3uofg1q0%40group.calendar.google.com/public/basic.ics");
-        final CalendarVisitor visitor = new OfficialCalendarVisitor();
-
-		final Calendar beforeCalendar = getCalendar(new File("src/test/resources/before.ics").toURI().toURL());
-        visitCalendar(beforeCalendar, visitor);
+		final CalendarManager manager = new CalendarManager();
+        manager.setVisitor(new OfficialCalendarVisitor());
+        manager.setUrl(new File("src/test/resources/before.ics").toURI().toURL());
+        manager.updateNow();
         
         final List<Event> beforeEvents = m_eventDao.findAll();
         assertEquals(2, beforeEvents.size());
@@ -59,8 +49,8 @@ public class CalendarTest {
         assertEquals("A", beforeEvents.get(0).getSummary());
         assertEquals("B", beforeEvents.get(1).getSummary());
 
-		final Calendar afterCalendar = getCalendar(new File("src/test/resources/after.ics").toURI().toURL());
-        visitCalendar(afterCalendar, visitor);
+		manager.setUrl(new File("src/test/resources/after.ics").toURI().toURL());
+        manager.updateNow();
         
         final List<Event> afterEvents = m_eventDao.findAll();
         assertEquals(2, afterEvents.size());
@@ -71,18 +61,19 @@ public class CalendarTest {
 	@Test
     public void testOfficialCalendarVisitorUpdateJccc2() throws Exception {
         // final URL url = new URL("https://www.google.com/calendar/ical/nh76o8dgn9d86b7n3p3uofg1q0%40group.calendar.google.com/public/basic.ics");
-        final CalendarVisitor visitor = new OfficialCalendarVisitor();
 
-		final Calendar beforeCalendar = getCalendar(new File("src/test/resources/jccc2-before.ics").toURI().toURL());
-        visitCalendar(beforeCalendar, visitor);
+		final CalendarManager manager = new CalendarManager();
+        manager.setVisitor(new OfficialCalendarVisitor());
+        manager.setUrl(new File("src/test/resources/jccc2-before.ics").toURI().toURL());
+        manager.updateNow();
         
         final List<Event> beforeEvents = m_eventDao.findAll();
         assertEquals(2, beforeEvents.size());
         assertEquals("DRAMA CLUB with Bill Corbett & Peter Sagal - Culinary Arts Center", beforeEvents.get(0).getSummary());
         assertEquals("JCCC2 \"Official\" Group Photo", beforeEvents.get(1).getSummary());
 
-		final Calendar afterCalendar = getCalendar(new File("src/test/resources/jccc2-after.ics").toURI().toURL());
-        visitCalendar(afterCalendar, visitor);
+		manager.setUrl(new File("src/test/resources/jccc2-after.ics").toURI().toURL());
+        manager.updateNow();
         
         final List<Event> afterEvents = m_eventDao.findAll();
         assertEquals(66, afterEvents.size());
@@ -92,32 +83,5 @@ public class CalendarTest {
         
         final Event deletedEvent = m_eventDao.get("9p9ueqvvu0t9moajheeodo4ros@google.com");
         assertNull(deletedEvent);
-	}
-
-	@SuppressWarnings("unchecked")
-	private void visitCalendar(final Calendar calendar, final CalendarVisitor visitor) {
-		visitor.begin();
-
-        for (final Component component : (List<Component>)calendar.getComponents()) {
-            String name = component.getName();
-
-            if ("VEVENT".equals(name)) {
-            	visitor.visitEvent(component);
-            } else if ("VTIMEZONE".equals(name)) {
-            	visitor.visitTimezone(component);
-            } else {
-                System.err.println("Warning: not sure how to handle " + name);
-            }
-        }
-
-        visitor.end();
-	}
-
-	private Calendar getCalendar(final URL url) throws IOException, ParserException {
-		final InputStream is = url.openStream();
-        final CalendarBuilder builder = new CalendarBuilder();
-        final Calendar calendar = builder.build(is);
-        is.close();
-		return calendar;
 	}
 }
