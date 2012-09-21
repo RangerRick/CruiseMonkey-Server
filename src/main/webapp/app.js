ef2ef14227efcfda3cdd1690e2b718f5c7e47fcf
Benjamin Reed <rangerrick@befunk.com>
2012-09-21 10:47:40 -0400

    auto-refreshing data from ReST

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -18,17 +18,47 @@ function Event(data) {
 	self.createdBy = ko.observable(data.createdBy);
 }
 
+var entryIdMatch = function(entry, id) {
+	return entry.id === id;
+}
+
 function EventsViewModel() {
 	var self = this;
 	self.events = ko.observableArray();
+
+	self.updateDataFromJSON = function() {
+	    $.getJSON("/events.json", function(allData) {
+	        var mappedTasks = $.map(allData.eventList, function(event) {
+	        	var item = ko.utils.arrayFirst(self.events(), function(entry) {
+	        		if (entry.id == event.id) {
+	        			// console.log("found match: " + ko.toJSON(entry));
+	        			return true;
+	        		} else {
+	        			return false;
+	        		}
+	        	});
+	        	if (item) {
+//	        		console.log("reusing " + ko.toJSON(item));
+	        		item.summary(event.summary + " (BLAH)");
+	        		item.description(event.description);
+	        		item.start(new Date(event.startDate));
+	        		item.end(new Date(event.endDate));
+	        		item.createdBy(event.createdBy);
+	        		return item;
+	        	} else {
+		        	return new Event(event);
+	        	}
+	        });
+	        self.events(mappedTasks);
+	    });    
+	};
 	
-    $.getJSON("/events.json", function(allData) {
-        var mappedTasks = $.map(allData.eventList, function(event) {
-        	return new Event(event);
-        });
-        self.events(mappedTasks);
-    });    
+	self.updateDataFromJSON();
 }
 
 var eventsModel = new EventsViewModel();
 ko.applyBindings(eventsModel);
+
+var interval = setInterval(function() {
+	eventsModel.updateDataFromJSON();
+}, 30000);
