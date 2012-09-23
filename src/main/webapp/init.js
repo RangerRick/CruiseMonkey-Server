4de48412b9d18efa8a3c9885df06f5bd669fb934
Benjamin Reed <rangerrick@befunk.com>
2012-09-23 14:07:49 -0400

    switch to using foundation

--- src/main/webapp/init.js
+++ src/main/webapp/init.js
@@ -31,8 +31,10 @@ var header, container;
 
 function setupDefaultView() {
     console.log("setting up default view");
-    
+
     showOfficialEventsView();
+
+    $(document).foundationTopBar();
 
     // initialize model data
     ko.applyBindings(eventsModel);
@@ -40,6 +42,17 @@ function setupDefaultView() {
     var interval = setInterval(function() {
     	eventsModel.updateDataFromJSON();
     }, 30000);
+
+
+    // Hide address bar on mobile devices
+    var Modernizr = window.Modernizr;
+    if (Modernizr.touch) {
+        $(window).load(function () {
+            setTimeout(function () {
+                window.scrollTo(0, 1);
+            }, 0);
+        });
+    }
 }
 
 function showOfficialEventsView() {
