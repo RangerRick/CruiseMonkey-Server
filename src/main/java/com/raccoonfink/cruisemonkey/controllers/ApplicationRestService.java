710fd6761c463bb814ac92076a240fd7eb76a1bf
Benjamin Reed <rangerrick@befunk.com>
2012-12-31 16:07:03 -0500

    use the services, not the daos

--- src/main/java/com/raccoonfink/cruisemonkey/controllers/ApplicationRestService.java
+++ src/main/java/com/raccoonfink/cruisemonkey/controllers/ApplicationRestService.java
@@ -21,11 +21,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import com.raccoonfink.cruisemonkey.dao.FavoriteDao;
-import com.raccoonfink.cruisemonkey.dao.UserDao;
 import com.raccoonfink.cruisemonkey.model.Event;
 import com.raccoonfink.cruisemonkey.model.EventCollection;
 import com.raccoonfink.cruisemonkey.server.EventService;
+import com.raccoonfink.cruisemonkey.server.FavoriteService;
+import com.raccoonfink.cruisemonkey.server.UserService;
 import com.sun.jersey.api.core.InjectParam;
 import com.sun.jersey.api.spring.Autowire;
 
@@ -41,31 +41,31 @@ public class ApplicationRestService extends RestServiceBase implements Initializ
 	@Autowired
 	EventService m_eventService;
 
-	@InjectParam("favoriteDao")
+	@InjectParam("favoriteService")
 	@Autowired
-	FavoriteDao m_favoriteDao;
+	FavoriteService m_favoriteService;
 
-	@InjectParam("userDao")
+	@InjectParam("userService")
 	@Autowired
-	UserDao m_userDao;
+	UserService m_userService;
 
 	@Context
 	UriInfo m_uriInfo;
 
 	public ApplicationRestService() {}
 
-	public ApplicationRestService(@InjectParam("eventService") final EventService eventService, @InjectParam("favoriteDao") final FavoriteDao favoriteDao, @InjectParam("userDao") final UserDao userDao) {
+	public ApplicationRestService(@InjectParam("eventService") final EventService eventService, @InjectParam("favoriteService") final FavoriteService favoriteService, @InjectParam("userService") final UserService userService) {
 		m_eventService = eventService;
-		m_favoriteDao = favoriteDao;
-		m_userDao = userDao;
+		m_favoriteService = favoriteService;
+		m_userService = userService;
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		super.afterPropertiesSet();
 		Assert.notNull(m_eventService);
-		Assert.notNull(m_favoriteDao);
-		Assert.notNull(m_userDao);
+		Assert.notNull(m_favoriteService);
+		Assert.notNull(m_userService);
 	}
 
 	@Transactional(readOnly=true)
@@ -86,9 +86,9 @@ public class ApplicationRestService extends RestServiceBase implements Initializ
 		m_logger.debug("{} events found", events == null? 0 : events.size());
 
 		final EventCollection collection = new EventCollection();
-		collection.setUser(m_userDao.get(userName));
+		collection.setUser(m_userService.getUser(userName));
 		collection.setEvents(events);
-		collection.setFavorites(m_favoriteDao.findByUser(userName));
+		collection.setFavorites(m_favoriteService.getFavorites(userName));
 
 		return collection;
 	}
