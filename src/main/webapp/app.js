d677b9d5f07adc335cb5dd52522f758172d46752
Benjamin Reed <rangerrick@befunk.com>
2013-01-07 08:54:40 -0500

    lots of refactoring

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -476,8 +476,8 @@ function Event(data) {
 	self.createdBy    = ko.observable(data["created-by"]);
 	self.owner        = ko.observable(data.owner);
 	self.timespan     = ko.computed(function() {
-		var start = start === null? null : formatTime(self.start(), false);
-		var end	= end	=== null? null : formatTime(self.end(), false);
+		var start = start === null? null : CMUtils.formatTime(self.start(), false);
+		var end	= end	=== null? null : CMUtils.formatTime(self.end(), false);
 		var retVal = "";
 		if (start !== null) {
 			retVal += start;
@@ -537,89 +537,6 @@ var matchEventText = function(event, filter) {
 };
 
 var serverModel = new ServerModel(m_isPhoneGap, amplify);
-
-function EventsViewModel() {
-	"use strict";
-
-	var self = this;
-	self.events = ko.observableArray();
-	self.updating = ko.observable(false);
-
-	self.updateData = function(allData) {
-		self.updating(true);
-		var favorites = [], dataFavorites = [], dataEvents = [];
-		if (!allData) {
-			console.log('EventsViewModel::updateData() called, but missing event data!');
-			return;
-		}
-		if (allData.favorites && allData.favorites.favorite) {
-			if (allData.favorites.favorite instanceof Array) {
-				dataFavorites = allData.favorites.favorite;
-			} else {
-				dataFavorites.push(allData.favorites.favorite);
-			}
-			favorites = $.map(dataFavorites, function(favorite) {
-				return favorite["@event"];
-			});
-		}
-		if (allData.events && allData.events.event) {
-			if (allData.events.event instanceof Array) {
-				dataEvents = allData.events.event;
-			} else {
-				dataEvents.push(allData.events.event);
-			}
-			console.log("EventsViewModel::updateData(): parsing " + dataEvents.length + " events");
-			var mappedTasks = $.map(dataEvents, function(event) {
-				"use strict";
-
-				var isFavorite, item;
-				if (event.favorite !== undefined) {
-					isFavorite = event.favorite;
-				} else {
-					isFavorite = (favorites.indexOf(event["@id"]) != -1);
-				}
-				// console.log("loading event: " + event['@id'] + ' (favorite = ' + isFavorite + ')');
-				item = ko.utils.arrayFirst(self.events(), function(entry) {
-					"use strict";
-
-					if (entry) {
-						if (entry.id() == event["@id"]) {
-							return true;
-						} else {
-							return false;
-						}
-					} else {
-						console.log("no entry");
-					}
-				});
-				if (item) {
-					var startDate = new Date(event.start);
-					var endDate	= new Date(event.end);
-					var createdBy = event["created-by"];
-
-					item.favorite(isFavorite);
-					if (item.summary()         != event.summary)       { item.summary(event.summary); }
-					if (item.description()     != event.description)   { item.description(event.description); }
-					if (item.start().getTime() != startDate.getTime()) { item.start(startDate); }
-					if (item.end().getTime()   != endDate.getTime())   { item.end(endDate); }
-					if (item.createdBy()       != createdBy)           { item.createdBy(createdBy); }
-					if (item.owner()           != event.owner)         { item.owner(event.owner); }
-					return item;
-				} else {
-					var e = new Event(event);
-					e.favorite(isFavorite);
-					return e;
-				}
-			});
-			self.events(mappedTasks);
-			amplify.store("events", allData);
-		} else {
-			console.log("no proper event data found: " + ko.toJSON(allData, null, 2));
-		}
-		// console.log("saving ReST events");
-		self.updating(false);
-	};
-}
 
 eventsModel = new EventsViewModel();
 
--- src/main/webapp/lib/cm_utils.js
+++ src/main/webapp/lib/cm_utils.js
@@ -28,8 +28,62 @@ var CMUtils = {
 		}
 		
 		return ( m_top >= window.pageYOffset );
-	}
+	},
 
+	days: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
+	months: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+
+	formatTime: function(d, doSeconds) {
+		"use strict";
+		var hour, ret;
+
+		hour = String('0' + (d.getHours() % 12)).slice(-2);
+		if (hour == '00') {
+			hour = '12';
+		}
+		ret = hour + ':' + String('0' + d.getMinutes()).slice(-2);
+
+		if (doSeconds === true) {
+			ret += ':' + String('0' + d.getSeconds()).slice(-2);
+		}
+
+		if ((d.getHours() % 12) == d.getHours()) {
+			ret += 'am';
+		} else {
+			ret += 'pm';
+		}
+		return ret;
+	},
+
+	formatDate: function(d) {
+		"use strict";
+		return this.days[d.getDay()] + ', ' + this.months[d.getMonth()] + ' ' + d.getDate();
+	},
+
+	getDateFromString: function(dateTime) {
+		"use strict";
+		if (dateTime instanceof Date) {
+			return new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate(), dateTime.getHours(), dateTime.getMinutes(), 0, 0);
+		}
+		var dateTimeParts = dateTime.split('T'),
+			dateParts = dateTimeParts[0].split('-'),
+			timeParts = dateTimeParts[1].split(':');
+
+		// console.log('year = ' + dateParts[0] + ', month = ' + dateParts[1] + ', day = ' + dateParts[2]);
+		// console.log('hours = ' + timeParts[0] + ', minutes = ' + timeParts[1]);
+
+		return new Date(dateParts[0], dateParts[1] - 1, dateParts[2], timeParts[0], timeParts[1], 0, 0);
+	},
+
+	padNumber: function(num) {
+		"use strict";
+		return (String('0' + num).slice(-2));
+	},
+
+	getStringFromDate: function(d) {
+		"use strict";
+		return d.getFullYear() + '-' + this.padNumber(d.getMonth() + 1) + '-' + this.padNumber(d.getDate()) + 'T' + this.padNumber(d.getHours()) + ':' + this.padNumber(d.getMinutes()) + ':' + this.padNumber(d.getSeconds()) + '-00:00';
+	}
 };
 
 (function () {
