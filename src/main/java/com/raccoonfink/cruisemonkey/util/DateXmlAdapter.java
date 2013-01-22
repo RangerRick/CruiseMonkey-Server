73e7332b3cd3d5f00db7fc4da8d018a2ba2b5e28
Benjamin Reed <rangerrick@befunk.com>
2013-01-22 15:02:43 -0500

    use the same time parser as the xml adapter

--- src/main/java/com/raccoonfink/cruisemonkey/util/DateXmlAdapter.java
+++ src/main/java/com/raccoonfink/cruisemonkey/util/DateXmlAdapter.java
@@ -7,16 +7,16 @@ import java.util.Date;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class DateXmlAdapter extends XmlAdapter<String, Date> {
-	final DateFormat m_format = new SimpleDateFormat("yyyy-MM-dd hh:mma");
+	public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mma");
 	@Override
 	public String marshal(final Date value) throws Exception {
-		return m_format.format(value);
+		return DATE_FORMAT.format(value);
 	}
 
 	@Override
 	public Date unmarshal(final String value) throws Exception {
 		if (value != null) {
-			return m_format.parse(value);
+			return DATE_FORMAT.parse(value);
 		}
 		return null;
 	}
