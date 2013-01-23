410a4303558f5d3ebc9690154271f14b3cea9671
Benjamin Reed <rangerrick@befunk.com>
2013-01-23 20:25:37 +0000

    set statusnet server to mine

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MAVEN_OPTS="-Xmx512m -server" mvn -Dmaven.test.skip.exec=true -Dorg.slf4j.simpleLogger.defaultLogLevel=debug install jetty:run 2>&1
+MAVEN_OPTS="-Xmx512m -server" mvn -Dmaven.test.skip.exec=true -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -DstatusNetHost=statusnet.raccoonfink.com install jetty:run 2>&1
