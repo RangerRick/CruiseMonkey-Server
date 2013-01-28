f747e27f28dfaae8f61c6909e7470f77baa5e39a
Benjamin Reed <rangerrick@befunk.com>
2013-01-27 21:56:52 -0500

    explicitly set the display timezone

--- src/main/java/com/raccoonfink/cruisemonkey/util/DateXmlAdapter.java
+++ src/main/java/com/raccoonfink/cruisemonkey/util/DateXmlAdapter.java
@@ -3,11 +3,16 @@ package com.raccoonfink.cruisemonkey.util;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class DateXmlAdapter extends XmlAdapter<String, Date> {
 	public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mma");
+	static {
+		DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("EST"));
+	}
+
 	@Override
 	public String marshal(final Date value) throws Exception {
 		return DATE_FORMAT.format(value);
